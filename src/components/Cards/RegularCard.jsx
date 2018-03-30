802b09ad5b875c00221d5061d504976ab03e8fa7
bonchevski <victor.bonchevski@gmail.com>
2018-03-30 12:29:15 +0300

    Added DefaultColor for header

--- src/components/Cards/RegularCard.jsx
+++ src/components/Cards/RegularCard.jsx
@@ -57,7 +57,7 @@ RegularCard.defaultProps = {
 RegularCard.propTypes = {
   plainCard: PropTypes.bool,
   classes: PropTypes.object.isRequired,
-  headerColor: PropTypes.oneOf(['orange', 'green', 'red', 'blue', 'purple','default']),
+  headerColor: PropTypes.oneOf(['orange', 'green', 'red', 'blue', 'purple', 'default']),
   cardTitle: PropTypes.node,
   cardSubtitle: PropTypes.node,
   content: PropTypes.node,

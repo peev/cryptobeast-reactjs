bc6bdcc369e48c2b5a32b1d2a8118b98d719f9f9
CosmicEon <sillimanite90@gmail.com>
2018-04-18 16:32:32 +0300

    added table to display

---
+++ src/components/CustomTables/PortfolioSummaryTable.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import {
+  withStyles,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+} from 'material-ui';
+
+import PropTypes from 'prop-types';
+import { inject, observer } from 'mobx-react';
+
+import tableStyle from '../../variables/styles/tableStyle';
+
+const data = [
+  [1, 2, 2, 2, 23, 4, 5, 3],
+  [1, 2, 2, 2, 23, 4, 5, 3],
+  [1, 2, 2, 2, 23, 4, 5, 3],
+];
+
+const styles = () => ({
+  warningText: {
+    marginTop: '35%',
+    textAlign: 'center',
+  },
+});
+
+const AllInvestorTable = inject('PortfolioStore')(observer(({ ...props }) => {
+  const { classes, tableHead, tableData, tableHeaderColor, PortfolioStore } = props;
+
+  const tableHeadContent = (
+    <TableHead className={classes[tableHeaderColor + 'TableHeader']}>
+      <TableRow>
+        {tableHead.map((prop, key) => {
+          return (
+            <TableCell
+              className={classes.tableCell + ' ' + classes.tableHeadCell}
+              key={key}
+            >
+              {prop}
+            </TableCell>
+          );
+        })}
+      </TableRow>
+    </TableHead>
+  );
+
+  let tableBodyContent;
+  console.log(PortfolioStore.selectedPortfolio);
+  if (PortfolioStore.selectedPortfolio) {
+    const currentAssets = PortfolioStore.selectedPortfolio.assets;
+    console.log(currentAssets);
+
+    tableBodyContent = currentAssets.map((el, key) => {
+      console.log(el, key);
+      return (
+        <TableRow key={key}>
+          {el.map((prop, i) => {
+            console.log(prop, i);
+            return (
+              <TableCell className={classes.tableCell} key={i}>
+                {prop}
+              </TableCell>
+            );
+          })}
+        </TableRow>
+      );
+    });
+    console.log(currentAssets);
+  }
+  console.log(tableBodyContent);
+
+  return (
+    <div className={classes.tableResponsive} >
+      <Table className={classes.table}>
+        {tableHead !== undefined ? tableHeadContent : null}
+        <TableBody>
+          {PortfolioStore.selectedPortfolio ? tableBodyContent : ''}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}))
+
+AllInvestorTable.defaultProps = {
+  tableHeaderColor: 'gray'
+};
+
+AllInvestorTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  tableHeaderColor: PropTypes.oneOf([
+    'warning',
+    'primary',
+    'danger',
+    'success',
+    'info',
+    'rose',
+    'gray'
+  ]),
+  tableHead: PropTypes.arrayOf(PropTypes.string),
+  tableData: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string))
+};
+
+export default withStyles(styles, tableStyle)(AllInvestorTable);

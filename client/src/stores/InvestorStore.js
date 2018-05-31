dfc469d139f97cd78271f99f835fddbe59d50f7c
Georgi Spasov <georgi.spasov@outlook.com>
2018-05-31 11:07:26 +0300

    Fixed default investor shares count

--- client/src/stores/InvestorStore.js
+++ client/src/stores/InvestorStore.js
@@ -412,7 +412,6 @@ class InvestorStore {
       telephone: '',
       dateOfEntry: (new Date()).toLocaleString(),
       managementFee: 0,
-      purchasedShares: this.convertedUsdEquiv, // Because initial share price = 1$
       portfolioId,
     };
 

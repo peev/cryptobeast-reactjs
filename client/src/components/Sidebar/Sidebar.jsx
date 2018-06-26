d745d330cd8ad290b4f62d752567ad7be6669627
CosmicEon <sillimanite90@gmail.com>
2018-06-26 15:03:15 +0300

    implemented new burger menu, refactored components

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,116 +2,53 @@
 import React from 'react';
 import uuid from 'uuid/v4';
 import { NavLink } from 'react-router-dom';
-import cx from 'classnames';
 import { withRouter } from 'react-router';
 import {
   withStyles,
-  Drawer,
-  Hidden,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
 } from 'material-ui';
-import sidebarStyle from './../../variables/styles/sidebarStyle';
+import SidebarStyle from './SidebarStyle';
 
 type Props = {
   classes: Object,
-  location: Object,
-  color: string,
-  image: string,
   routes: Array<string>,
-  handleDrawerToggle: Function,
-  open: boolean,
   disabled: boolean,
+  closed: boolean,
 };
 
 const Sidebar = ({ ...props }: Props) => {
-  // verifies if routeName is the one active (in browser input)
-  function activeRoute(routeName: string) {
-    return props.location.pathname.indexOf(routeName) > -1;
-  }
-  const { classes, color, image, routes, disabled } = props;
-
-  const links = (
-    <List className={classes.list}>
-      {routes.map((prop: Object) => {
-        const listItemClasses = cx({
-          [` ${classes[color]}`]: activeRoute(prop.path),
-        });
-
-        const whiteFontClasses = cx({
-          [` ${classes.whiteFont}`]: activeRoute(prop.path),
-        });
-
-        return (
-          <NavLink
-            to={prop.path}
-            className={classes.item}
-            activeClassName="active"
-            key={uuid()}
-          >
-            <ListItem
-              button
-              className={classes.itemLink + listItemClasses}
-              disabled={disabled}
-            >
-              <ListItemIcon className={classes.itemIcon + whiteFontClasses}>
-                <prop.icon />
-              </ListItemIcon>
-              <ListItemText
-                primary={prop.sidebarName}
-                className={classes.itemText + whiteFontClasses}
-                disableTypography
-              />
-            </ListItem>
-          </NavLink>
-        );
-      })}
-    </List>
-  );
+  const { classes, routes, disabled, closed } = props;
 
   return (
-    <div>
-      <Hidden mdUp>
-        <Drawer
-          variant="temporary"
-          anchor="left"
-          open={props.open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          onClose={props.handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
+    <List className={classes.list}>
+      {routes.map((prop: Object) => (
+        <ListItem
+          button
+          className={classes.itemLink}
+          disabled={disabled}
+          key={uuid()}
         >
-
-          <div className={classes.sidebarWrapper}>
-            {links}
-          </div>
-          {image !== undefined ? (
-            <div
-              className={classes.background}
+          <NavLink
+            to={prop.path}
+            className={`${classes.item} ${!closed ? classes.itemClosed : ''}`}
+            activeClassName="active"
+          >
+            <ListItemIcon className={classes.itemIcon}>
+              <prop.icon />
+            </ListItemIcon>
+            <ListItemText
+              primary={prop.sidebarName}
+              className={classes.itemText}
+              disableTypography
             />
-          ) : null}
-        </Drawer>
-      </Hidden>
-
-      <Hidden smDown>
-        <Drawer
-          anchor="left"
-          variant="permanent"
-          open
-          classes={{
-            paper: `${classes.drawerPaper} ${(disabled ? classes.disableOverlay : '')}`,
-          }}
-        >
-          <div className={classes.sidebarWrapper}>{links}</div>
-        </Drawer>
-      </Hidden>
-    </div>
+          </NavLink>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
-export default withStyles(sidebarStyle)(withRouter(Sidebar));
+export default withStyles(SidebarStyle)(withRouter(Sidebar));

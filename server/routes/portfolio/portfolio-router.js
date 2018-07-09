c8c6246a57e4602447aee022b7b85f17c89f7924
Ivan Parushev <i.p.parushev@gmail.com>
2018-07-09 11:11:15 +0300

    backend validation

--- server/routes/portfolio/portfolio-router.js
+++ server/routes/portfolio/portfolio-router.js
@@ -2,19 +2,20 @@ const { Router } = require('express');
 
 const attachTo = (app, repository, jobs) => {
   const router = new Router();
-  const portfolioController = require('./portfolio-controller')(repository, jobs);
+  const controller = require('./portfolio-controller')(repository, jobs);
+  const validator = require('./portfolio-validator')();
 
   router
-    .post('/create', (req, res) => portfolioController.createPortfolio(req, res))
-    .get('/all', (req, res) => portfolioController.getAllPortfolios(req, res))
-    .get('/:portfolioId/:item', (req, res) => portfolioController.searchItemsInCurrentPortfolio(req, res))
-    .put('/update/:id', (req, res) => portfolioController.updatePortfolio(req, res))
-    .delete('/delete/:id', (req, res) => portfolioController.removePortfolio(req, res))
-    .post('/updatePortfolioCost', (req, res) => portfolioController.updatePortfolioBTCEquivalentOnRequest(req, res))
-    .post('/getPortfolioSharePrice', (req, res) => portfolioController.getPortfolioSharePrice(req, res))
-    .post('/sharePriceHistory', (req, res) => portfolioController.getSharePriceHistory(req, res))
-    .post('/priceHistory', (req, res) => portfolioController.getPricesHistory(req, res))
-    .post('/periodPriceHistory', (req, res) => portfolioController.getPricesHistoryForPeriod(req, res));
+    .post('/create', validator.createPortfolio, controller.createPortfolio)
+    .get('/all', (req, res) => controller.getAllPortfolios(req, res))
+    .get('/:portfolioId/:item', (req, res) => controller.searchItemsInCurrentPortfolio(req, res))
+    .put('/update/:id', validator.createPortfolio, controller.updatePortfolio)
+    .delete('/delete/:id', (req, res) => controller.removePortfolio(req, res))
+    .post('/updatePortfolioCost', (req, res) => controller.updatePortfolioBTCEquivalentOnRequest(req, res))
+    .post('/getPortfolioSharePrice', (req, res) => controller.getPortfolioSharePrice(req, res))
+    .post('/sharePriceHistory', (req, res) => controller.getSharePriceHistory(req, res))
+    .post('/priceHistory', (req, res) => controller.getPricesHistory(req, res))
+    .post('/periodPriceHistory', (req, res) => controller.getPricesHistoryForPeriod(req, res));
 
   app.use('/portfolio', router);
 };

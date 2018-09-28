12f7ab2d612a55d8f02650338e355085d9bdf19d
DEVOLO\Dimitar.Jilanov <djilanov@gmail.com>
2018-09-28 17:16:00 +0300

    Small improvements over the tabs

--- client/src/components/Tabs/AnalyticsItems/Performance.jsx
+++ client/src/components/Tabs/AnalyticsItems/Performance.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 import { withStyles, Grid } from '@material-ui/core';
 import { inject, observer } from 'mobx-react';
 
-import Button from '../../CustomButtons/Button';
+import MotionSelect from '../../Selectors/MotionSelect';
 import SelectBenchmark from '../../Selectors/Analytics/SelectBenchmark';
-import SelectPeriod from '../../Selectors/Analytics/SelectPeriod';
 import TotalAssetsValue from '../../HighCharts/TotalAssetsValue';
 import PerformanceAssets from '../../HighCharts/PerformanceAssets';
 import SummaryPerformanceCard from '../../Cards/Analytics/SummaryPerformanceCard';
@@ -38,58 +37,62 @@ type Props = {
   classes: Object,
 };
 
-const Performance = inject('Analytics')(observer(({ ...props }: Props) => {
-  const { classes, Analytics } = props;
+@inject('Analytics')
+@observer
+class Performance extends React.Component<Props, State> {
 
-  const handleSelectTimePeriod = () => {
-    if (Analytics.selectedTimeInPerformance === '') {
-      console.log('selected period is needed');
+  constructor(props) {
+    super(props);
+    this.handleSelectPeriod = this.handleSelectPeriod.bind(this);
+  }
+
+  handleSelectPeriod = (data) => {
+    const { Analytics } = this.props;
+
+    if (!data) {
       return;
     }
 
+    Analytics.selectTimeInPerformance(data);
     Analytics.getPortfolioPriceHistoryForTimePeriod();
   };
 
-  return (
-    <Grid container>
-      <Grid container className={classes.header}>
-        <Grid item xs={2} sm={2} md={2} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
-          <SelectPeriod />
-        </Grid>
+  render() {
+    const { classes } = this.props;
 
-        <Grid item xs={1} sm={1} md={1} className={[classes.flex, classes.flexBottom].join(' ')}>
-          <Button onClick={() => handleSelectTimePeriod()}>Apply</Button>
-        </Grid>
-      </Grid>
-
+    return (
       <Grid container>
-        <Grid item xs={8} sm={8} md={8} className={classes.marginRight}>
-          <TotalAssetsValue />
+        <Grid container className={classes.header}>
+          <Grid item xs={2} sm={2} md={2} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
+            <MotionSelect defaultValueIndex={0} selectedValue={this.handleSelectPeriod} values={['1d', '1w', '1m']} />
+          </Grid>
         </Grid>
 
-        <Grid item xs={3} sm={3} md={3}>
-          <SummaryPerformanceCard />
+        <Grid container>
+          <Grid item xs={8} sm={8} md={8} className={classes.marginRight}>
+            <TotalAssetsValue />
+          </Grid>
+
+          <Grid item xs={3} sm={3} md={3}>
+            <SummaryPerformanceCard />
+          </Grid>
         </Grid>
+
+        <Grid container>
+          <Grid item xs={2} sm={2} md={2} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
+            <SelectBenchmark />
+          </Grid>
+        </Grid>
+
+        <Grid container>
+          <Grid item xs={12} sm={12} md={12}>
+            <PerformanceAssets />
+          </Grid>
+        </Grid>
+
       </Grid>
-
-      <Grid container>
-        <Grid item xs={2} sm={2} md={2} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
-          <SelectBenchmark />
-        </Grid>
-
-        <Grid item xs={1} sm={1} md={1} className={[classes.flex, classes.flexBottom].join(' ')}>
-          <Button>Apply</Button>
-        </Grid>
-      </Grid>
-
-      <Grid container>
-        <Grid item xs={12} sm={12} md={12}>
-          <PerformanceAssets />
-        </Grid>
-      </Grid>
-
-    </Grid>
-  );
-}));
+    );
+  }
+};
 
 export default withStyles(styles)(Performance);
--- client/src/components/Tabs/AnalyticsItems/Volatility.jsx
+++ client/src/components/Tabs/AnalyticsItems/Volatility.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { withStyles, Grid } from '@material-ui/core';
-import { inject, observer } from 'mobx-react';
 
-import Button from '../../CustomButtons/Button';
 import SelectBenchmark from '../../Selectors/Analytics/SelectBenchmark';
-import SelectPeriod from '../../Selectors/SelectPeriod';
+import MotionSelect from '../../Selectors/MotionSelect';
 import VolatilityColumnChart from '../../HighCharts/VolatilityDevSkew';
 import VolatilityAndRisk from '../../HighCharts/VolatilityAndRisk';
 import VolatilityTable from '../../CustomTables/VolatilityTable';
@@ -68,61 +66,74 @@ type Props = {
   classes: Object,
 };
 
-const Volatility = inject('Analytics')(observer(({ ...props }: Props) => {
-  const { classes } = props;
+class Volatility extends React.Component<Props, State> {
 
-  return (
-    <Grid container className={classes.overflowNone}>
-      <Grid container>
-        <Grid item xs={3} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
-          <SelectPeriod />
-        </Grid>
+  constructor(props) {
+    super(props);
+    this.handleSelectPeriod = this.handleSelectPeriod.bind(this);
+  }
 
-        <Grid item xs={3} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
-          <SelectBenchmark />
-        </Grid>
+  handleSelectPeriod(data) {
+    if (!data) {
+      return;
+    }
+    this.setState({
+      selectPeriod: data
+    });
+  }
 
-        <Grid item xs={1} className={[classes.flex, classes.flexBottom].join(' ')}>
-          <Button>Apply</Button>
-        </Grid>
-      </Grid>
+  render() {
+    const { classes } = this.props;
 
-      <Grid container className={classes.smallTopPadding}>
-        <Grid item xs={7} className={[classes.topItem, classes.topHeight, classes.leftTopItem].join(' ')}>
-          <Paper className={classes.topHeight}>
-            <VolatilityTable style={{ width: '100%' }}  className={[classes.flex, classes.flexCenter].join(' ')} />
-          </Paper>
-        </Grid>
-
-        <Grid item xs={5} className={[classes.topItem, classes.topHeight].join(' ')}>
-          <Paper className={classes.padding}>
-            <VolatilitySpider style={{ width: '100%' }} />
-          </Paper>
-        </Grid>
-      </Grid>
-
-      <Grid container className={classes.bigTopPadding}>
-        <Paper className={['VolatilityPaper', classes.maxWidth, classes.padding].join(' ')}>
-          <h5 className={classes.noMargin}>PORTFOLIO VOLATILITY AND RISK</h5>
-
-          <Grid container>
-            <Grid item xs={3}><p>STANDARD DEVIATION: <b>2.63</b></p></Grid>
-            <Grid item xs={3}><p>PORTFOLIO ALPHA: <b>2.14%</b></p></Grid>
-            <Grid item xs={3}><p>PORTFOLIO BETA: <b>0.65%</b></p></Grid>
-            <Grid item xs={3}><p>PORTFOLIO VARIANCE: <b>0.65</b></p></Grid>
+    return (
+      <Grid container className={classes.overflowNone}>
+        <Grid container>
+          <Grid item xs={3} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
+            <MotionSelect defaultValueIndex={0} selectedValue={this.handleSelectPeriod} values={['1d', '1w', '1m']} />
           </Grid>
 
-          <VolatilityAndRisk />
-        </Paper>
-      </Grid>
+          <Grid item xs={3} className={[classes.marginRight, classes.flex, classes.flexCenter].join(' ')}>
+            <SelectBenchmark />
+          </Grid>
+        </Grid>
 
-      <Grid container className={classes.bigTopPadding}>
-        <Paper className={[classes.maxWidth, classes.padding].join(' ')}>
-          <VolatilityColumnChart />
-        </Paper>
+        <Grid container className={classes.smallTopPadding}>
+          <Grid item xs={7} className={[classes.topItem, classes.topHeight, classes.leftTopItem].join(' ')}>
+            <Paper className={classes.topHeight}>
+              <VolatilityTable style={{ width: '100%' }} className={[classes.flex, classes.flexCenter].join(' ')} />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={5} className={[classes.topItem, classes.topHeight].join(' ')}>
+            <Paper className={classes.padding}>
+              <VolatilitySpider style={{ width: '100%' }} />
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Grid container className={classes.bigTopPadding}>
+          <Paper className={['VolatilityPaper', classes.maxWidth, classes.padding].join(' ')}>
+            <h5 className={classes.noMargin}>PORTFOLIO VOLATILITY AND RISK</h5>
+
+            <Grid container>
+              <Grid item xs={3}><p>STANDARD DEVIATION: <b>2.63</b></p></Grid>
+              <Grid item xs={3}><p>PORTFOLIO ALPHA: <b>2.14%</b></p></Grid>
+              <Grid item xs={3}><p>PORTFOLIO BETA: <b>0.65%</b></p></Grid>
+              <Grid item xs={3}><p>PORTFOLIO VARIANCE: <b>0.65</b></p></Grid>
+            </Grid>
+
+            <VolatilityAndRisk />
+          </Paper>
+        </Grid>
+
+        <Grid container className={classes.bigTopPadding}>
+          <Paper className={[classes.maxWidth, classes.padding].join(' ')}>
+            <VolatilityColumnChart />
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
-  );
-}));
+    );
+  }
+};
 
 export default withStyles(styles)(Volatility);

bc2d6a82c3ac8a56bd255d9613716eb7d2b5af9a
DEVOLO\Dimitar.Jilanov <djilanov@gmail.com>
2018-10-15 18:20:09 +0300

    Set the currency price sync

--- server/routes/wei-fiat-fx/wei-fiat-fx-controller.js
+++ server/routes/wei-fiat-fx/wei-fiat-fx-controller.js
@@ -1,4 +1,6 @@
+const { weiFiatFxService } = require('./wei-fiat-fx-service');
 const { responseHandler } = require('../utilities/response-handler');
+const { krakenServices } = require('../../integrations/kraken-services');
 const { etherScanServices } = require('../../integrations/etherScan-services');
 
 const modelName = 'WeiFiatFx';
@@ -8,22 +10,7 @@ const weiFiatFxController = (repository) => {
 
   const createWeiFiatFx = async (req, res) => {
     const weiFiatFxData = req.body;
-    let priceUsdValue;
-
-    const ethPrice = await etherScanServices().getETHUSDPrice();
-
-    switch (weiFiatFxData.fxName) {
-      case 'ETH':
-        priceUsdValue = ethPrice;
-        break;
-      default:
-        await marketService.getTickersFromKraken(`XETHZ${weiFiatFxData.fxName}`)
-          .then((data) => {
-            priceUsdValue = data[0].last / ethPrice;
-          })
-          .catch(err => console.log(err));
-        break;
-    }
+    let priceUsdValue = await weiFiatFxService.getPriceByType(weiFiatFxData.fxName)
 
     const newWeiFiatFxObject = {
       fxName: weiFiatFxData.fxName,
@@ -111,9 +98,26 @@ const weiFiatFxController = (repository) => {
       .then(result => responseHandler(res, result))
       .catch(error => res.json(error));
   };
-
+  
   const sync = (data) => {
-    
+    repository.find({ modelName }).then((currencies) => {
+      // Update the usd price of all currencies
+      currencies.forEach((currency) => {
+        let priceUsdValue = await weiFiatFxService.getPriceByType(currency.fxName);
+        repository.update(
+          { 
+            modelName, 
+            updatedRecord: {
+              id: currency.id
+              priceUSD: priceUsdValue
+            } 
+          })
+          .then((response) => {
+            // TODO: Handle the response based on all items on all controllers ready
+          })
+          .catch(error => res.json(error));
+      });
+    });
   };
 
   return {

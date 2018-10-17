9d3b7910cb0341e9c3c8ae8b5e7e253fd54e3c46
TeodorKolev <teodor.kolev@silicon-nile.com>
2018-10-17 17:50:06 +0300

    wei currency table before test sync

--- server/routes/wei-currency/wei-currency-controller.js
+++ server/routes/wei-currency/wei-currency-controller.js
@@ -1,69 +1,89 @@
 const { responseHandler } = require('../utilities/response-handler');
-const { WeidexService } = require('../../services/weidex-service');
+// const { WeidexService } = require('../../services/weidex-service');
 
 const modelName = 'WeiCurrency';
 
 const weiCurrencyController = (repository) => {
-  const createWeiCurrency = async (req, res) => {
-    const weiCurrencyData = req.body;
-    // const today = Math.floor(Date.now() / 1000);
-    // const yesterday = today - (24 * 3600);
-    // const lastWeek = today - (24 * 7 * 3600);
+  const WeidexService = require('../../services/weidex-service')(repository);
 
-    const priceResponse = await WeidexService.getTokenTicker(weiCurrencyData.id);
+  const getCurrencyObject = async (req) => {
+    const today = Math.floor(Date.now() / 1000);
+    const yesterday = today - (24 * 3600);
+    const lastWeek = today - (24 * 17 * 3600);
+    let currencyDayStatsJson = null;
+    let currencyWeekStatsJson = null;
+    let volume24HStats = 0;
+    let high24HStats = 0;
+    let low24HStats = 0;
+    let change24HStats = 0;
+    let change7DStats = 0;
 
-    // TODO uncomment whan staging has tickers
-    // const currencyDayStats = await WeidexService.getCurrencyStats(weiCurrencyData.id, yesterday, today);
-    // const currencyWeekStats = await WeidexService.getCurrencyStats(weiCurrencyData.id, lastWeek, today);
-    // const currencyDayStatsJson = JSON.parse(currencyDayStats);
-    // const currencyWeekStatsJson = JSON.parse(currencyWeekStats);
+    const priceResponse = await WeidexService.getTokenTicker(req.id);
+    const currencyDayStats = await WeidexService.getCurrencyStats(req.id, yesterday, today);
+    const currencyWeekStats = await WeidexService.getCurrencyStats(req.id, lastWeek, today);
 
-    // let volume24HStats = 0;
-    // let high24HStats = 0;
-    // let low24HStats = 0;
-    // let change24HStats = 0;
-    // let change7DStats = 0;
+    if (!Array.isArray(currencyDayStats)) {
+      currencyDayStatsJson = JSON.parse(currencyDayStats);
+    } else {
+      currencyDayStatsJson = currencyDayStats;
+    }
 
-    // if (currencyDayStatsJson.length) {
-    //   volume24HStats = currencyDayStatsJson.volume;
-    //   high24HStats = currencyDayStatsJson.high24H;
-    //   low24HStats = currencyDayStatsJson.low24H;
-    //   change24HStats = Number((currencyDayStatsJson[0].close - currencyDayStatsJson[0].open) / currencyDayStatsJson[0].open);
-    // }
+    if (!Array.isArray(currencyWeekStats)) {
+      currencyWeekStatsJson = JSON.parse(currencyWeekStats);
+    } else {
+      currencyWeekStatsJson = currencyWeekStats;
+    }
 
-    // if (currencyWeekStatsJson.length) {
-    //   if (currencyWeekStatsJson.length === 1) {
-    //     change7DStats = Number((currencyWeekStatsJson[0].close - currencyWeekStatsJson[0].open) / currencyWeekStatsJson[0].open);
-    //   } else {
-    //     change7DStats = Number((
-    //       currencyWeekStatsJson[currencyWeekStatsJson.length - 1].close - currencyWeekStatsJson[0].open) / currencyWeekStatsJson[0].open);
-    //   }
-    // }
+    if (currencyDayStatsJson.length > 0) {
+      volume24HStats = currencyDayStatsJson.volume;
+      high24HStats = currencyDayStatsJson.high24H;
+      low24HStats = currencyDayStatsJson.low24H;
+      change24HStats = Number((currencyDayStatsJson[0].close - currencyDayStatsJson[0].open)
+      / currencyDayStatsJson[0].open);
+    }
+
+    if (currencyWeekStatsJson.length > 0) {
+      if (currencyWeekStatsJson.length === 1) {
+        change7DStats = Number((currencyWeekStatsJson[0].close - currencyWeekStatsJson[0].open)
+         / currencyWeekStatsJson[0].open);
+      } else {
+        change7DStats = Number((currencyWeekStatsJson[currencyWeekStatsJson.length - 1].close - currencyWeekStatsJson[0].open)
+        / currencyWeekStatsJson[0].open);
+      }
+    }
 
     const newWeiCurrencyObject = {
-      tokenId: weiCurrencyData.id,
-      tokenName: weiCurrencyData.name,
-      tokenNameLong: weiCurrencyData.fullName,
+      tokenId: req.id,
+      tokenName: req.name,
+      tokenNameLong: req.fullName,
       lastPriceETH: priceResponse.price,
-      // volume24H: volume24HStats,
-      // high24H: high24HStats,
-      // low24H: low24HStats,
-      // change24H: change24HStats,
-      // change7D: change7DStats,
+      volume24H: volume24HStats,
+      high24H: high24HStats,
+      low24H: low24HStats,
+      change24H: change24HStats,
+      change7D: change7DStats,
       // bid: priceResponse.body.bid,
       // ask: priceResponse.body.ask,
     };
+
+    return newWeiCurrencyObject;
+  };
+
+  const createWeiCurrency = async (req, res) => {
+    const weiCurrency = req.body;
+
+    const weiCurrencyObject = await getCurrencyObject(weiCurrency);
 
     await repository.findOne({
       modelName,
       options: {
         where: {
-          tokenName: weiCurrencyData.name,
+          tokenName: weiCurrency.name,
         },
       },
     }).then((currency) => {
       if (currency === null) {
-        repository.create({ modelName, newObject: newWeiCurrencyObject })
+        repository.create({ modelName, newObject: weiCurrencyObject })
           .then((response) => {
             res.status(200).send(response);
           })
@@ -71,7 +91,7 @@ const weiCurrencyController = (repository) => {
             res.json(error);
           });
       } else {
-        const newWeiCurrencyData = Object.assign({}, newWeiCurrencyObject, { id: currency.id });
+        const newWeiCurrencyData = Object.assign({}, weiCurrencyObject, { id: currency.id });
         repository.update({ modelName, updatedRecord: newWeiCurrencyData })
           .then((response) => {
             res.status(200).send(response);
@@ -96,20 +116,10 @@ const weiCurrencyController = (repository) => {
 
   const updateWeiCurrency = async (req, res) => {
     const { id } = req.params;
-    const weiCurrencyData = req.body;
 
-    const priceResponse = await WeidexService.getTokenTicker(weiCurrencyData.id);
+    const weiCurrency = await getCurrencyObject(req);
 
-    const newWeiCurrencyObject = {
-      tokenId: weiCurrencyData.id,
-      tokenName: weiCurrencyData.name,
-      tokenNameLong: weiCurrencyData.fullName,
-      lastPriceETH: priceResponse.price,
-      // bid: priceResponse.body.bid,
-      // ask: priceResponse.body.ask,
-    };
-
-    const newWeiCurrencyData = Object.assign({}, newWeiCurrencyObject, { id });
+    const newWeiCurrencyData = Object.assign({}, weiCurrency, { id });
     repository.update({ modelName, updatedRecord: newWeiCurrencyData })
       .then((response) => {
         res.status(200).send(response);
@@ -124,33 +134,11 @@ const weiCurrencyController = (repository) => {
       .catch(error => res.json(error));
   };
 
-  const sync = (data) => {
-    const tokens = WeidexService.getAllTokens().then(res => res.json());
-    tokens.forEach((token) => {
-      const ticker = WeidexService.getTokenTicker(token.id).then(res => res.json());
-      // TODO: Get the stats from the non public api
-      repository.update({
-        modelName,
-        updatedRecord: {
-          tokenId: token.id,
-          tokenName: token.name,
-          tokenNameLong: token.fullName,
-          // volume24H: volume24HStats,
-          // high24H: high24HStats,
-          // low24H: low24HStats,
-          // change24H: change24HStats,
-          // change7D: change7DStats,
-          lastPriceETH: ticker.price,
-          bid: ticker.bid,
-          ask: ticker.ask,
-        }
-      })
-        .then((response) => {
-          res.status(200).send(response);
-        })
-        .catch(error => res.json(error));
+  const sync = async () => {
+    const tokens = await WeidexService.getAllTokens().then(res => res.json());
+    tokens.forEach(async (token) => {
+      await createWeiCurrency(token);
     });
-
   };
 
   return {
@@ -158,7 +146,7 @@ const weiCurrencyController = (repository) => {
     getWeiCurrency,
     updateWeiCurrency,
     removeWeiCurrency,
-    sync
+    sync,
   };
 };
 

54fbc67231e7cc4483f6a67d566ee8a1584265be
Ivan Parushev <i.p.parushev@gmail.com>
2018-10-25 09:48:41 +0300

    health check endpoint && Jenkins pipeline optimization

---
+++ server/routes/utilities/health-check-router.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const attachTo = (app, repository, jobs) => {
+  const router = new Router();
+
+  router
+    .get('/check', (req, res) => {
+      res.status(200).json({ status: 'ok' });
+    })
+    
+
+  app.use('/health', router);
+};
+
+module.exports = { attachTo };

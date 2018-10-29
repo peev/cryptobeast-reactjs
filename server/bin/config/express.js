dbdde2666b2a10d06483d57638553d6dcef33bf5
Dimitar Jilanov <DJilanov@gmail.com>
2018-10-29 15:23:04 +0200

    Update express.js

--- server/bin/config/express.js
+++ server/bin/config/express.js
@@ -96,7 +96,6 @@ const init = async (repository) => {
   require('./../../routes/transaction/transaction-router').attachTo(app, repository, jobs);
   require('./../../routes/trade-history/trade-history-router').attachTo(app, repository, jobs);
   require('./../../routes/utilities/health-check-router').attachTo(app, repository);
-  require('./../../routes/main/main-router').attachTo(app, repository, jobs);
 
   // Handle Errors
   // catch 404 and forward to error handler

8738392f6e36bbededddc6dd1a9114caa1f03a4b
TeodorKolev <teodor.kolev@silicon-nile.com>
2018-11-19 17:46:48 +0200

    228 working on balance calculations

--- server/routes/portfolio/portfolio-controller.js
+++ server/routes/portfolio/portfolio-controller.js
@@ -5,6 +5,7 @@ const modelName = 'Portfolio';
 const portfolioController = (repository) => {
   const portfolioService = require('../../services/portfolio-service')(repository);
   const WeidexService = require('../../services/weidex-service')(repository);
+  const BigNumberService = require('../../services/big-number-service')(repository);
 
   const getPortfolioObject = async address => repository.findOne({
     modelName,
@@ -23,6 +24,26 @@ const portfolioController = (repository) => {
         userAddress: address,
       },
       include: [{ all: true }],
+    },
+  })
+    .catch(err => console.log(err));
+
+  const getAllocations = async portfolioIdParam => repository.find({
+    modelName: 'Allocation',
+    options: {
+      where: {
+        portfolioID: portfolioIdParam,
+      },
+    },
+  })
+    .catch(err => console.log(err));
+
+  const getCurrency = async tokenNameParam => repository.findOne({
+    modelName: 'Currency',
+    options: {
+      where: {
+        tokenName: tokenNameParam,
+      },
     },
   })
     .catch(err => console.log(err));
@@ -163,6 +184,68 @@ const portfolioController = (repository) => {
       .catch(error => res.json(error));
   };
 
+  const getEndOfDay = (timestamp) => {
+    const firstDate = new Date(timestamp);
+    firstDate.setHours(23);
+    firstDate.setMinutes(59);
+    firstDate.setSeconds(59);
+    return new Date(firstDate.toString()).getTime();
+  };
+
+  const getStartOfDay = (timestamp) => {
+    const firstDate = new Date(timestamp);
+    firstDate.setHours(0);
+    firstDate.setMinutes(0);
+    firstDate.setSeconds(0);
+    return new Date(firstDate.toString()).getTime();
+  };
+
+  const calculateDays = (begin, end) => {
+    const dates = [];
+    for (let i = begin; i <= end; i += (24 * 60 * 60 * 1000)) {
+      dates.push(i);
+    }
+    return dates;
+  };
+
+  const getLastBalance = (start, end, allocations) => {
+    allocations.filter(allocation =>
+      new Date(allocation.timestamp.toString()).getTime() >= start && new Date(allocation.timestamp.toString()).getTime() <= end);
+    return (allocations[allocations.length - 1] !== undefined) ? allocations[allocations.length - 1].balance : undefined;
+  };
+
+  const sumBalance = async (req, res, timestamp, balance) =>
+    (balance.reduce(async (acc, item) => {
+      const currency = await getCurrency(item.tokenName);
+      const value = await WeidexService.getTokenValueByTimestampHttp(Number(currency.tokenId), timestamp)
+        .then(data => ((data.length > 0) ? data : 0))
+        .catch(err => res.status(500).send(err));
+      const product = BigNumberService.product(item.balance, value);
+      const final = await (BigNumberService.sum(acc, product), 0);
+      return final;
+    }));
+
+  const getPortfolioValueHistory = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const allocations = await getAllocations(id);
+      const today = new Date().getTime();
+      const yesterday = today - (24 * 60 * 60 * 1000);
+      const begin = new Date((allocations[0].timestamp).toString()).getTime();
+      const days = calculateDays(getEndOfDay(begin), getEndOfDay(yesterday));
+      const final = days.map(async (day, index) => {
+        const start = getStartOfDay(day);
+        const item = getLastBalance(start, day, allocations);
+        const balanceSum = await sumBalance(req, res, day, item);
+        return Promise.resolve(item !== undefined ? { timestamp: day, balance: balanceSum } : final[index - 1]);
+      });
+      await Promise.all(final)
+        .then(data => res.status(200).send(data));
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  };
+
   const sync = async (req, res, addresses) => {
     const portfolioArray = addresses.map(async (address) => {
       try {
@@ -187,6 +270,7 @@ const portfolioController = (repository) => {
     sync,
     getPortfoliosByAddresses,
     getPortfolioAssetsByPortfolioId,
+    getPortfolioValueHistory,
   };
 };
 

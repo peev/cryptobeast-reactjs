5c291c72b57d3dc8088fc106ae0c921c09d66147
TeodorKolev <teodor.kolev@silicon-nile.com>
2018-11-20 16:41:15 +0200

    228 and 229 finished

--- client/src/components/HighCharts/ProfitLoss.jsx
+++ client/src/components/HighCharts/ProfitLoss.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 
@@ -10,47 +10,60 @@ type Props = {
   days: Object,
 };
 
-const ProfitLossChart = (observer(({ currency, chartData, days }: Props) => {
-  console.log(chartData);
-  const options = {
-    chart: {
-      type: 'column',
-    },
-    title: {
-      text: `${currency} DAILY PROFIT AND LOSS`,
-    },
-    xAxis: {
-      categories: days,
-      type: 'category',
-      min: 0,
-      scrollbar: {
-        enabled: true,
+
+@inject('currency', 'chartData', 'days')
+@observer
+class ProfitLossChart extends React.Component<Props, State> {
+  componentDidUpdate() {
+    const chart = this.refs.chart;
+    if (chart !== undefined) {
+      chart.chart.xAxis[0].setExtremes(0, (this.props.chartData.length > 32) ? 30 : this.props.chartData.length);
+    }
+  }
+
+  render() {
+    const { currency, days, chartData } = this.props;
+    const options = {
+      chart: {
+        type: 'column',
       },
-      tickLength: 0,
-    },
-    yAxis: {
       title: {
-        text: 'Profit and Loss in %',
+        text: `${currency} DAILY PROFIT AND LOSS`,
       },
-    },
-    legend: {
-      enabled: false,
-    },
-    credits: {
-      enabled: false,
-    },
-    series: [{
-      name: currency,
-      data: chartData,
-      color: Highcharts.getOptions().colors[2],
-    }],
-  };
-  return (
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={options}
-    />
-  );
-}));
+      xAxis: {
+        categories: days,
+        type: 'category',
+        min: 0,
+        scrollbar: {
+          enabled: true,
+        },
+        tickLength: 0,
+      },
+      yAxis: {
+        title: {
+          text: 'Profit and Loss in %',
+        },
+      },
+      legend: {
+        enabled: false,
+      },
+      credits: {
+        enabled: false,
+      },
+      series: [{
+        name: currency,
+        data: chartData,
+        color: Highcharts.getOptions().colors[2],
+      }],
+    };
+    return (
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        ref={'chart'}
+      />
+    );
+  }
+}
 
 export default ProfitLossChart;

82d183ec43d795aaa2a5692b266e97dd53b203a7
TeodorKolev <teodor.kolev@silicon-nile.com>
2018-11-22 18:41:12 +0200

    250 working on basics

--- server/routes/investor/investor-router.js
+++ server/routes/investor/investor-router.js
@@ -7,8 +7,6 @@ const attachTo = (app, data) => {
 
   router
     .post('/add', validator.createInvestor, controller.createInvestor)
-    .put('/deposit/', validator.investorTransaction, controller.depositInvestor)
-    .put('/withdrawal/', validator.investorTransaction, controller.withdrawalInvestor)
     .put('/update/:id', validator.updateInvestor, controller.updateInvestor)
     .delete('/delete', validator.deleteInvestor, controller.removeInvestor);
 

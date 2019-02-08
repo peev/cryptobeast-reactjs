9f2a6c84a10c81647f6accb6cf4c9194380efe79
TeodorKolev <teodorkolev007@gmail.com>
2019-02-08 16:16:43 +0200

    298 added dummy crons

--- server/services/market-service.js
+++ server/services/market-service.js
@@ -1,5 +1,6 @@
 const { CronJob } = require('../node_modules/cron');
 const cron = require('node-cron');
+const requester = require('../services/requester-service');
 const { coinMarketCapServices } = require('../integrations/coinMarketCap-services');
 
 const marketService = (repository) => {
@@ -27,10 +28,47 @@ const marketService = (repository) => {
       return marketFunction();
     });
 
+  const dummyNodeCron = time =>
+    cron.schedule(time, () => {
+      console.log(`DUMMY NODE-CRON ON EVERY 10 MINS FROM 03: ${new Date()}`);
+    });
+
+  const dummyCron = time =>
+    new CronJob(time, () => {
+      console.log(`DUMMY CRON ON EVERY 10 MINS FROM 04: ${new Date()}`);
+    }, null, true);
+
+  const dummy = () => new Promise((resolve, reject) => {
+    requester.get('https://reqres.in/api/users?page=2')
+      .then((response) => {
+        console.log(`RESPONSE FROM DUMMY CRON: ${response}`);
+        const parsedResult = JSON.parse(response);
+        resolve(parsedResult);
+      })
+      .catch(err => reject(err)); // eslint-disable-line
+  });
+
+  const dummyRequestNodeCron = (marketFunction, time) =>
+    cron.schedule(time, () => {
+      console.log(`DUMMY REQUEST NODE-CRON ON EVERY 10 MINS FROM 05: ${new Date()}`);
+      return marketFunction();
+    });
+
+  const dummyRequestCron = (marketFunction, time) =>
+    new CronJob(time, () => {
+      console.log(`DUMMY REQUEST CRON ON EVERY 10 MINS FROM 06: ${new Date()}`);
+      return marketFunction();
+    }, null, true);
+
   return {
     syncTickersFromCoinMarketCap,
     createCronJob,
     createNodeCron,
+    dummyCron,
+    dummyNodeCron,
+    dummy,
+    dummyRequestNodeCron,
+    dummyRequestCron,
   };
 };
 
